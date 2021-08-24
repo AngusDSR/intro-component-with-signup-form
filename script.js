286da1125977971306b3e01a521a8315c783b79f
Cowfresh <51661234+Cowfresh@users.noreply.github.com>
2021-08-24 22:41:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,35 +4,43 @@ const errorText = document.createElement("P");
 
 form.addEventListener('submit', (e)=> {
 	e.preventDefault();
-	resetErrorMessages();
 	formValidator()
 });
 
 function formValidator () {
-
 	for (let field of formInputs) {
+		field.addEventListener('input', (e)=> {
+			field.nextElementSibling.firstElementChild.innerHTML = ''
+			field.classList.remove("input-error");
+			field.nextElementSibling.nextElementSibling.classList.remove("input-error");
+		});
+
 		if (field.validity.valueMissing) {	
-			field.nextElementSibling.innerHTML = field.ariaLabel + ' cannot be empty';
-			field.classList.add("input-error");
+			field.nextElementSibling.firstElementChild.innerHTML = field.ariaLabel + ' cannot be empty';
 			field.nextElementSibling.nextElementSibling.classList.add("input-error");
+			field.classList.add("input-error");
 
 		} else if (field.validity.typeMismatch) {
-			field.nextElementSibling.innerHTML = 'Looks like this is not an ' + field.type;
+			field.nextElementSibling.firstElementChild.innerHTML = 'Looks like this is not an ' + field.type;
+			field.nextElementSibling.nextElementSibling.classList.add("input-error");
 			field.classList.add("input-error");
 
 		} else if (field.validity.tooShort) {
-			field.nextElementSibling.innerHTML = field.ariaLabel + ' must be minimum 8 characters';
+			field.nextElementSibling.firstElementChild.innerHTML = field.ariaLabel + ' must be minimum 8 characters';
+			field.nextElementSibling.nextElementSibling.classList.add("input-error");
 			field.classList.add("input-error");
 
 		} else {
-			// submit form
+			// [submit form]
 		}
 	}
 };
 
-function resetErrorMessages () {
+function reValidate () {
 	for (let field of formInputs) {
-		field.classList.remove("input-error");
-		field.nextElementSibling.innerHTML = '';
-	}	
+		field.addEventListener('focusout', (e) => {
+			console.log()
+		})
+	}
+
 }
